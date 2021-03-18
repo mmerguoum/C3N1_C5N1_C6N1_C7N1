0a1328d6a50def783ea4acb4412c925f5720be92
mmerguoum <merguoummourad@gmail.com>
2021-03-18 15:58:45 +0100

    brief-form

---
+++ Brief_C3N1_C5N1_C6N1_C7N1/Brief-form/helpers/login/login.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="maincop.css">
+    <title>Login in</title>
+</head>
+<body>
+<form action="connexion.php" method="POST">  
+ 
+ <div class="container">
+     
+ <h2 class="login">Login</h2><br>
+ <input type="email" name="email" placeholder="E-mail" class="email" ><br>
+ <input type="password" name="password" placeholder="Password" class="password"><br>
+ <input type="submit" name="enter" class="btn" value="Envoyer"><br>
+ <a href= "../registre/registre.php" class = "save">inscrivez-vous</a>
+</body>
+</html>
