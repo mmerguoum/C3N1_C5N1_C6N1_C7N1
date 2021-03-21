6d59c2ebc96375a675a0c9df1b1ff993d8aa8c14
mmerguoum <merguoummourad@gmail.com>
2021-03-21 19:33:34 +0100

    sur files

--- Brief_C3N1_C5N1_C6N1_C7N1/Brief-form/helpers/registre/config.php
+++
@@ -1,8 +0,0 @@
- <?php
-// include './helpers/functions.php';
-$con = mysqli_connect("localhost","root","","brief") or die("Error:can't connect to server ");
-
-// $con = mysqli_connect("localhost","root","") or die("Error:can't connect to server");
-// $db = mysqli_select_db($con,"brief")  or die("Error:can't connect to database");
-
-?> 
